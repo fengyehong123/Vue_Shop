73224bd601b533f9a507e04017592e815f02a942
fengyehong123 <1355930128@qq.com>
2020-10-28 22:05:26 +0800

    day01

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
+// 按需导入我们需要用到的组件
 import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
+// Vue.use() 注册为全局可用的组件
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
