382d63beca277c2ee53f36d3c9786080466fb877
fengyehong123 <1355930128@qq.com>
2020-11-11 21:51:32 +0800

    day05

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -42,7 +42,7 @@
                 <el-table-column label="操作">
                     <template>
                         <el-button size="mini" type="primary" icon="el-icon-edit" @click="showBox"></el-button>
-                        <el-button size="mini" type="success" icon="el-icon-location"></el-button>
+                        <el-button size="mini" type="success" icon="el-icon-location" @click="showProgressBox"></el-button>
                     </template>
                 </el-table-column>
             </el-table>
@@ -84,6 +84,22 @@
                 <el-button type="primary" @click="addressVisible = false">确 定</el-button>
             </span>
         </el-dialog>
+
+        <!-- 展示物流进度的对话框 -->
+        <el-dialog
+        title="物流进度"
+        :visible.sync="progressVisible"
+        width="50%">
+            <!-- Timeline时间线组件 -->
+            <el-timeline>
+                <el-timeline-item
+                v-for="(activity, index) in progressInfo"
+                :key="index"
+                :timestamp="activity.time">
+                {{activity.context}}
+                </el-timeline-item>
+            </el-timeline>
+        </el-dialog>
     </div>
 </template>
 
@@ -120,7 +136,73 @@ export default {
                 ]
             },
             // 省市区/县数据对象
-            cityData: cityData
+            cityData: cityData,
+            // 物流进度的对话框展示与否
+            progressVisible: false,
+            // 物流数据对象
+            progressInfo: [
+                {
+                    "time": "2018-05-10 09:39:00",
+                    "ftime": "2018-05-10 09:39:00",
+                    "context": "已签收,感谢使用顺丰,期待再次为您服务",
+                    "location": ""
+                },
+                {
+                    "time": "2018-05-10 08:23:00",
+                    "ftime": "2018-05-10 08:23:00",
+                    "context": "[北京市]北京海淀育新小区营业点派件员 顺丰速运 95338正在为您派件",
+                    "location": ""
+                },
+                {
+                    "time": "2018-05-10 07:32:00",
+                    "ftime": "2018-05-10 07:32:00",
+                    "context": "快件到达 [北京海淀育新小区营业点]",
+                    "location": ""
+                },
+                {
+                    "time": "2018-05-10 02:03:00",
+                    "ftime": "2018-05-10 02:03:00",
+                    "context": "快件在[北京顺义集散中心]已装车,准备发往 [北京海淀育新小区营业点]",
+                    "location": ""
+                },
+                {
+                    "time": "2018-05-09 23:05:00",
+                    "ftime": "2018-05-09 23:05:00",
+                    "context": "快件到达 [北京顺义集散中心]",
+                    "location": ""
+                },
+                {
+                    "time": "2018-05-09 21:21:00",
+                    "ftime": "2018-05-09 21:21:00",
+                    "context": "快件在[北京宝胜营业点]已装车,准备发往 [北京顺义集散中心]",
+                    "location": ""
+                },
+                {
+                    "time": "2018-05-09 13:07:00",
+                    "ftime": "2018-05-09 13:07:00",
+                    "context": "顺丰速运 已收取快件",
+                    "location": ""
+                },
+                {
+                    "time": "2018-05-09 12:25:03",
+                    "ftime": "2018-05-09 12:25:03",
+                    "context": "卖家发货",
+                    "location": ""
+                },
+                {
+                    "time": "2018-05-09 12:22:24",
+                    "ftime": "2018-05-09 12:22:24",
+                    "context": "您的订单将由HLA（北京海淀区清河中街店）门店安排发货。",
+                    "location": ""
+                },
+                {
+                    "time": "2018-05-08 21:36:04",
+                    "ftime": "2018-05-08 21:36:04",
+                    "context": "商品已经下单",
+                    "location": ""
+                }
+            ],
+
         }
     },
     created() {
@@ -158,12 +240,21 @@ export default {
         addressDialogClosed() {
             // 关闭对话框时,清空表单中的数据对象的数据
             this.$refs.addressFormRef.resetFields()
+        },
+        // 展示物流进度信息
+        async showProgressBox() {
+            // 让对话框显示
+            this.progressVisible = true
         }
     }
 }
 </script>
 
 <style lang="less" scoped>
+// 收到导入时间线组件的样式
+@import '../../plugins/timeline/timeline.css';
+@import '../../plugins/timeline-item/timeline-item.css';
+
 .el-cascader {
     width: 100%;
 }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -40,6 +40,10 @@ import {
     Upload
 } from 'element-ui'
 
+// 目前项目中使用的elementUI版本过于老旧,没有Timeline时间组件,此处是把新版本的时间组件单独放到项目中,然后在此处导入
+import Timeline from './timeline/index.js'
+import TimelineItem from './timeline-item/index.js'
+
 // Vue.use() 注册为全局可用的组件
 Vue.use(Button)
 Vue.use(Form)
@@ -76,6 +80,8 @@ Vue.use(Step)
 Vue.use(Checkbox)
 Vue.use(CheckboxGroup)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 // $message是我们自定义的属性名称,Message是我们导入的组件的名称
 // 下面的代码代表把弹窗组件挂载到Vue的原型对象上,这样的话每一个组件都可以通过this访问到$message
