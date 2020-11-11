27a5946edee741305605dd4b5011fa681be02123
fengyehong123 <1355930128@qq.com>
2020-11-11 22:20:51 +0800

    day05

--- src/router.js
+++ src/router.js
@@ -22,6 +22,8 @@ import GoodsList from './components/goods/List.vue'
 import Add from './components/goods/Add.vue'
 // 导入订单模块
 import Order from './components/order/Order.vue'
+// 导入报表组件
+import Report from './components/report/Report.vue'
 
 Vue.use(Router)
 
@@ -52,7 +54,8 @@ const router = new Router({
         { path: '/params', component: Params },
         { path: '/goods', component: GoodsList },
         { path: '/goods/add', component: Add },
-        { path: '/orders', component: Order }
+        { path: '/orders', component: Order },
+        { path: '/reports', component: Report }
       ]
     },  
   ]
